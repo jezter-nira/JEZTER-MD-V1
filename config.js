8d23184840790f27d8bac23c7931e6d97351d173
jezter-nira <charithniroshana7@gmail.com>
2025-06-26 13:58:55 -0600

    Update config.js

--- config.js
+++ config.js
@@ -7,3 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 };
+ALIVE_IMG: process.env.ALIVE_IMG || "https://pin.it/1rVorQgLF",
+ALIVE_MS: process.env.ALIVE_MSG || "Hello I'm Alive Now jezter xmd bot Alive now make by jezter",
+
